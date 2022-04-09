5f4126b8c1003fa20b6c161e72ee7a9630a177a3
denimcouch <alexmata92@ymail.com>
2022-04-09 08:23:33 -0500

    add styling to header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,13 @@
 import PropTypes from 'prop-types'
 
-function Header(props) {
-  const { text } = props
+function Header({ text, bgColor, textColor }) {
+  const headerStyles = {
+    backgroundColor: bgColor,
+    color: textColor
+  }
+
   return (
-    <header>
+    <header style={ headerStyles }>
       <div className="container headerContainer">
         <h1 className="header__title">
           {text}
@@ -15,10 +19,14 @@ function Header(props) {
 
 Header.defaultProps = {
   text: 'Feedback UI',
+  bgColor: 'rgba(0, 0, 0, .4)',
+  textColor: '#ff6a95'
 }
 
 Header.propTypes = {
-  text: PropTypes.string
+  text: PropTypes.string,
+  bgColor: PropTypes.string,
+  textColor: PropTypes.string,
 }
 
 export default Header
