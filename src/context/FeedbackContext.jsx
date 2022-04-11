c04dca048bf5b88501659d7b03fe75a4beb5a01d
denimcouch <alexmata92@ymail.com>
2022-04-10 22:18:31 -0500

    fetching data from the json server

--- src/context/FeedbackContext.jsx
+++ src/context/FeedbackContext.jsx
@@ -1,20 +1,26 @@
-import { createContext, useState } from 'react'
+import { createContext, useState, useEffect } from 'react'
 
 const FeedbackContext = createContext()
 
 export const FeedbackProvider = ({ children }) => {
-  const [feedback, setFeedback] = useState([
-    {
-      id: 10,
-      text: 'This is coming from context',
-      rating: 10,
-    },
-  ])
-
+  const [loading, setLoading] = useState(true)
+  const [feedback, setFeedback] = useState([])
   const [feedbackEdit, setFeedbackEdit] = useState({
     item: {},
     edit: false,
   })
+
+  useEffect(() => {
+    fetchFeedback()
+  }, [])
+
+  const fetchFeedback = async () => {
+    const data = await fetch(
+      'http://localhost:5050/feedback?_sort=id&order=desc'
+    ).then((res) => res.json())
+    setFeedback(data)
+    setLoading(false)
+  }
 
   const addFeedback = (feedbackObj) => {
     const postOptions = {
@@ -48,11 +54,17 @@ export const FeedbackProvider = ({ children }) => {
     setFeedback(
       feedback.map((item) => (item.id === id ? { ...item, ...updItem } : item))
     )
+    setFeedbackEdit({
+      ...feedbackEdit,
+      edit: false,
+      item: {},
+    })
   }
 
   return (
     <FeedbackContext.Provider
       value={{
+        loading,
         feedback,
         feedbackEdit,
         addFeedback,
